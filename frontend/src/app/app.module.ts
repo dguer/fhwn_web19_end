d5189387ce50145f687d8e2cf108fd6fe411df62
Dilber Gür <d.gur@gmx.de>
2020-01-17 12:30:44 +0100

    start

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ScoreComponent } from './score/score.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    GameComponent,
+    NavbarComponent,
+    ScoreComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
